56b930cafc714f1f44e723e19704d42710dfddea
steviebdesignstraining <47852623+steviebdesignstraining@users.noreply.github.com>
2025-07-19 19:59:30 +0100

    Cypress update
    Fixed cypress.config.js to display feature scenario, still need to be fixed.

--- Cypress/cypress.config.js
+++ Cypress/cypress.config.js
@@ -1,26 +1,35 @@
-const { defineConfig } = require('cypress');
-const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
-const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
-const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild');
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-require('dotenv').config();
-
-module.exports = defineConfig({
+import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+// import '@shelex/cypress-allure-plugin';
+export default defineConfig({
   e2e: {
-    stepDefinitions: ["cypress/e2e/api"],
-    specPattern: 'cypress/e2e/api/*.feature',
-    supportFile: 'cypress/support/e2e.js',
-    setupNodeEvents: async (on, config) => {
+    // specPattern: "cypress/e2e/*.feature",
+    // stepDefinitions: "cypress/e2e/step_definitions/*.js",
+    async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
-      on('file:preprocessor', createBundler({
-        plugins: [createEsbuildPlugin.default(config)]
-      }));
-      allureWriter(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
       return config;
     },
-    // specPattern: "cypress/e2e/api/*.feature",
-    env: {
-      BASE_URL: process.env.BASE_URL
-    }
-  }
+    // 🔽 This tells the plugin where to look for step definitions
+    env: { // This 'env' block is specifically for cypress-cucumber-preprocessor settings
+      "cypress-cucumber-preprocessor": {
+        stepDefinitions: [
+          "cypress/e2e/step_definitions/*.js", // This path
+          "cypress/e2e/*.feature",              // Or a broader glob
+          // "cypress/support/step_definitions/*.js", // Include support folder
+          // Add any other custom paths where your step definitions reside
+        ],
+        baseUrl: process.env.BASE_URL || "https://testapi.io/api/ottplatform/media",
+
+        // nonGlobalStepDefinitions: true, // You might need this depending on your structure
+      },
+    },
+  },
 });
---
+++ Cypress/cypress/e2e/step_definitions/media.js
@@ -0,0 +1,54 @@
+import { Given, Then } from '@badeball/cypress-cucumber-preprocessor';
+import { getMediaAPI } from '../../support/api'; // Adjust the path as necessary
+let response;
+
+Given("I send a GET request to the media API", () => {
+  cy.request("/media").then((res) => {
+    response = res;
+  });
+});
+
+
+Given("I send a GET request to mediax", () => {
+  cy.getMediaAPI('x').then(({ response }) => {
+    apiResult.response = response;
+  });
+});
+
+Then("the response status code should be {int}", (statusCode) => {
+  expect(response.status).to.eq(statusCode);
+});
+
+
+Then("the response time should be below {int} ms", (max) => {
+  expect(apiResult.duration).to.be.lessThan(max);
+});
+
+Then("all items should have a non-empty id", () => {
+  mediaData.forEach((item) => {
+    expect(item.id).to.exist;
+    expect(item.id).to.not.be.empty;
+  });
+});
+
+Then('only one item should have "now_playing" offset as true', () => {
+  const nowPlaying = mediaData.filter(item => item.offset?.now_playing === true);
+  expect(nowPlaying.length).to.eq(1);
+});
+
+Then("the response should contain a Date header", () => {
+  expect(apiResult.response.headers).to.have.property('date');
+});
+
+Then("the response should contain {int} items", (count) => {
+  expect(mediaData).to.have.length(count);
+});
+
+Then("there should be only one item with now_playing true", () => {
+  const nowPlaying = mediaData.filter(item => item.offset?.now_playing === true);
+  expect(nowPlaying.length).to.eq(1);
+});
+
+Then("the response should contain a list of media items", () => {
+  expect(response.body).to.have.property("media").and.to.be.an("array");
+});
